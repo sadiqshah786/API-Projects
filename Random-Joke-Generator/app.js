0d187da7785d9d3821b0bd25360a6ecf878e4f37
Sadiq Shah <52124323+sadiqshah786@users.noreply.github.com>
2022-12-05 15:19:39 +0500

    Add files via upload
    Random Quotes Generator - Using https://jokeapi.dev/ API

--- Random-Joke-Generator/app.js
+++ Random-Joke-Generator/app.js
@@ -1,31 +1,32 @@
 let jokeTitle = document.querySelector('.card-text');
 let cat = document.querySelector('.cat');
 let btn = document.querySelector('.btn');
-
-
-btn.addEventListener("click",async ()=>{
+btn.addEventListener("click", async () => {
     btn.classList.add("Loading")
     btn.innerHTML = "Loading Joke..."
     await fetch('https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,political,racist,sexist,explicit')
-    .then((res)=>{
-        return res.json();
-    })
-    .then((data)=>{
-        console.log(data)
-        if(data.joke){
-            jokeTitle.innerHTML = `${data.joke}`
-            cat.innerHTML =  `━ ${data.category}`
-            btn.classList.remove("Loading")
-            btn.innerHTML = "Next Joke"
-        }
-        else{
-            jokeTitle.innerHTML = `${data.setup}`
-            cat.innerHTML =  `━ ${data.category}`
-            btn.classList.remove("Loading")
-            btn.innerHTML = "Next Joke"
-        }
-    })
-    .catch((error)=>{
-        jokeTitle.innerHTML =  error
-    })
+        .then((res) => {
+
+            return res.json();
+        })
+        .then((data) => {
+
+            if (data.joke) {
+                jokeTitle.innerHTML = `${data.joke}`
+                cat.innerHTML = `━ ${data.category}`
+                btn.classList.remove("Loading")
+                btn.innerHTML = "Next Joke"
+            }
+
+            else {
+                jokeTitle.innerHTML = `${data.setup}`
+                cat.innerHTML = `━ ${data.category}`
+                btn.classList.remove("Loading")
+                btn.innerHTML = "Next Joke"
+            }
+        })
+
+        .catch((error) => {
+            jokeTitle.innerHTML = "SomeThing Went Wrong..."
+        })
 })
